f775137c597adadcb51fb5496e83712d21b6250f
Lonnie Coffman <lcoffman@mobilesoft.com>
2020-07-05 11:35:44 -0700

    user create and get routes

---
+++ controllers/users/users_controller.go
@@ -0,0 +1,43 @@
+package users
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/LonnieCoffman/bookstore_users-api/domain/users"
+	"github.com/LonnieCoffman/bookstore_users-api/services"
+	"github.com/LonnieCoffman/bookstore_users-api/utils/errors"
+	"github.com/gin-gonic/gin"
+)
+
+// CreateUser ..
+func CreateUser(c *gin.Context) {
+	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("Invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+	c.JSON(http.StatusCreated, result)
+}
+
+// GetUser ..
+func GetUser(c *gin.Context) {
+	userID, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if userErr != nil {
+		err := errors.NewBadRequestError("user ID should be a number")
+		c.JSON(err.Status, err)
+		return
+	}
+	user, getErr := services.GetUser(userID)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
